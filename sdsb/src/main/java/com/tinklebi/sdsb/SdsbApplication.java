5dcb783a7cbd96e7f98da64938a9be200b1c8e54
Sprevon <sevenuniversity@outlook.com>
2024-01-27 22:16:41 +0800

    feat:project init

---
+++ sdsb/src/main/java/com/tinklebi/sdsb/SdsbApplication.java
@@ -0,0 +1,16 @@
+package com.tinklebi.sdsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Seven
+ */
+@SpringBootApplication
+public class SdsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdsbApplication.class, args);
+    }
+
+}
---
+++ sdsb/src/test/java/com/tinklebi/sdsb/SdsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinklebi.sdsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
