afed5a39da7e7fffab7c379aa1f3eaa4fd7e6fb2
Sprevon <sevenuniversity@outlook.com>
2024-03-20 14:12:18 +0800

    fix:修改bugs

--- src/main/java/com/tinklebi/sdsb/business/playerheartrate/entity/po/PlayerHeartRateData.java
+++ src/main/java/com/tinklebi/sdsb/business/playerheartrate/entity/po/PlayerHeartRateData.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class PlayerHeartRateData {
     private static final long serialVersionUID = -4688282826116547256L;
 
+    private int plId;
     private PlayerHeartRateDb playerHeartRateDb;
     private List<PlayerHeartRate> heartRateList;
 }
